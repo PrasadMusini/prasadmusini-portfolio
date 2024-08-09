14a454c54ee5674b4f241200c535a1403160bb5e
PrasadMusini <prasadmusini1999@gmail.com>
2024-08-10 00:58:44 +0530

    fixed issue in index.html

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2111392195"
+    serviceWorkerVersion: "2614727828"
   }
 });
